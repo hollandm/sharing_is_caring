e82bab8c767c09260b9daf5bbe6488c70ed05f07
mbholland333@gmail.com <mbholland333@gmail.com@fd6833e1-3d2e-1144-e6b9-005187d2ea30>
2013-03-22 00:22:58 +0000

    test

--- SIC/src/network/NetworkManager.java
+++ SIC/src/network/NetworkManager.java
@@ -1,5 +1,11 @@
 package network;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+import java.net.Socket;
+
 /**
  * Scans inbound network traffic for SIC data
  * 
@@ -17,6 +23,30 @@ public class NetworkManager implements Runnable{
 	@Override
 	public void run() {
 
+		try {
+			MulticastSocket listener = new MulticastSocket (9001);
+			
+			byte[] dataIN = new byte[7];
+			
+			DatagramPacket recv = new DatagramPacket(dataIN, 7);
+			InetAddress add = InetAddress.getByName("224.0.0.1");
+			listener.joinGroup(add);
+			
+			listener.setReceiveBufferSize(7);
+			
+			System.out.println("Listening to traffic");
+			
+			while (true) {
+				listener.receive(recv);
+				
+				
+			}
+			
+//			listener.close();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		
 		
 	}
