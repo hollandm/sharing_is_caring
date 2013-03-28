9c199c21a048bfa059ab6503269b178b410b1c07
mbholland333@gmail.com <mbholland333@gmail.com@fd6833e1-3d2e-1144-e6b9-005187d2ea30>
2013-03-28 03:13:49 +0000

    send data stuff

--- SIC/src/network/UpdateThread.java
+++ SIC/src/network/UpdateThread.java
@@ -1,9 +1,16 @@
 package network;
 
 import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
 import java.net.InetSocketAddress;
+import java.net.MulticastSocket;
 import java.net.Socket;
+
+import file.FileIO;
 
 /**
  * UpdateThread is used when a client requests an revision update.
@@ -32,8 +39,56 @@ public class UpdateThread implements Runnable{
 	}
 
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
+		
+		FileIO giveMeData = new FileIO();
+
+		File file = new File("E:/Dropbox/Sophmor Spring Semester/CS 445/test.exe");
+		
+		byte[] data = giveMeData.readFile(file);
+		
+		byte[] send = new byte[SicNetworkProtocol.dataPacketSize];
+		
+		for (int i = 0; i < SicNetworkProtocol.dataPacketHeaderSize; i++) {
+			send[i] = 0;
+		}
+		
+		
+		MulticastSocket sock = new MulticastSocket(SicNetworkProtocol.port);
+		
+		InetAddress addr = InetAddress.getByName("224.0.0.1");
+//		InetSocketAddress addr = new InetSocketAddress("10.12.18.26", SicNetworkProtocol.port);
+//		sock.connect(addr);
+		DatagramPacket datagram = new DatagramPacket(send, SicNetworkProtocol.dataPacketSize,addr,SicNetworkProtocol.port);
+		
+		int segmentCount = 0;
+		int sentAmount = 0;
+		int fileSize = data.length;
+		
+		
+		while (fileSize > sentAmount) {
+
+			int remainingData = SicNetworkProtocol.dataPacketSize-SicNetworkProtocol.dataPacketHeaderSize;
+			for (int i = 0; i < remainingData/8; ++i) {
+				if (fileSize < sentAmount+i) {
+					byte a = data[sentAmount+i];
+					send[SicNetworkProtocol.dataPacketHeaderSize+i] = a;
+				} else {
+					send[SicNetworkProtocol.dataPacketHeaderSize+i] = 0;
+				}
+			}
+			send[SicNetworkProtocol.dataPacketHeaderSize-1] = (byte) segmentCount;
+			
+			sentAmount += remainingData;
+			
+			segmentCount++;
+			sock.send(datagram);
+			System.out.println("Sent segment: "+segmentCount);
+			
+			
+			
+		}
 	}
 	
 	
