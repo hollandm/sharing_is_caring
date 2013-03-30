3d0d7c3296e35cd98ec010bb4d49f5f192ca4bc8
mbholland333@gmail.com <mbholland333@gmail.com@fd6833e1-3d2e-1144-e6b9-005187d2ea30>
2013-03-30 09:10:26 +0000

    I can copy files via multi-cast. However the reason why I can't send things to another computer still eludes me...

--- SIC/src/network/SicNetworkProtocol.java
+++ SIC/src/network/SicNetworkProtocol.java
@@ -1,12 +1,12 @@
 package network;
 
+import java.net.InetAddress;
+
 /**
  * SicNetworkProtocol is a simple class to store
  * information about the protocol in a single
  * location in order to simplify modifications
  * down the road
- * 
- * TODO: dear god please change the protocol strings
  * 
  * TODO: flesh out protocol with the other things we said we would do
  */
@@ -15,9 +15,17 @@ public final class SicNetworkProtocol {
 
 	public static final int port = 7532;
 	
-	public static final int cmdPacketSize = 10;		//size of command size 
-	public static final int dataPacketSize = 100;	//maximum size of a data packet
+	public static final int cmdPacketSize = 100;		//size of command size 
 	
+	
+	public static final int dataPacketHeaderSize = 13;	//Data packet header size
+		//first byte says its a data packet
+		//next 4 bytes go to revision #
+		//next 4 bytes goes to file #
+		//next 4 bytes goes to fragment #
+	public static final int dataPacketSize = 1000;	//maximum size of a data packet
+	public static final int dataPacketDataCapacity = dataPacketSize - dataPacketHeaderSize;	//how much left over space is there with header
+
 	
 	/**
 	 * first byte in data represents whether or not the packet is a command or a piece of data
@@ -34,8 +42,12 @@ public final class SicNetworkProtocol {
 	 * requestRevision:	sender is indicating that receiver should send out
 	 * 					an update
 	 */
-	public static final byte pushRevision = 1;
-	public static final byte requestRevision = 2;
+	public static final byte pushRevision = 1;		//[4 bytes: revision #][4 bytes: # of files]
+	public static final byte requestRevision = 2;	//[4 bytes: requested revision #]
+	
+	public static final byte startFile = 50;		//[4 bytes: size of file in bytes][95 bytes: file path]
+	
+	public static final byte requestFragment = 70;	//[4 bytes: file #][4 bytes: fragment #]
 	
 	
 	
