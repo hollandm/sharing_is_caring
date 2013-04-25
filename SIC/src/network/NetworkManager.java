85870b7a70ca7f8194fbf07c4775e27a07162807
lilvietboi04@gmail.com <lilvietboi04@gmail.com@fd6833e1-3d2e-1144-e6b9-005187d2ea30>
2013-04-25 07:41:48 +0000

    Clears the list of files modified and the list of files removed

--- SIC/src/network/NetworkManager.java
+++ SIC/src/network/NetworkManager.java
@@ -57,6 +57,7 @@ public class NetworkManager {
 				}
 				
 				if(SicNetworkProtocol.getCmdType(cmdIN) == SicNetworkProtocol.pullRevision) {
+					System.out.println("HERE WE ARE");
 					uploader.initateUpload(components.dirMonitor.getFilesChanged(), 
 							components.dirMonitor.getFilesRemoved(), components.settings.getDirectory(),
 							components.settings.getRevision());
--- SIC/src/network/SicUploader.java
+++ SIC/src/network/SicUploader.java
@@ -132,7 +132,7 @@ public class SicUploader {
 			
 		}
 		
-
+		components.dirMonitor.clearVectors();
 	}
 	
 	
