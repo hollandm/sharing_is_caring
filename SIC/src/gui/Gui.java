fae1672dc3b9337532dfac4e0881257e9c8b0ee4
sherryliao_1@yahoo.com <sherryliao_1@yahoo.com@fd6833e1-3d2e-1144-e6b9-005187d2ea30>
2013-04-26 07:36:58 +0000

    components.netManager.changeGroup() added

--- SIC/src/gui/Gui.java
+++ SIC/src/gui/Gui.java
@@ -32,14 +32,20 @@ import Main.SicComponents;
 
 import state.Settings;
 
+/**
+ * Gui
+ * 
+ * This class creates the home gui, folder gui, and delay gui. It serves as the
+ * listener for all of the buttons and menu items for each of the gui's.
+ *
+ */
 public class Gui implements ActionListener{
-	
+
 	protected SicComponents components;
 
 	protected JFrame myFrame = new JFrame();
-	
+
 	protected int versionID = 0;
-		
 
 	/**
 	 * Instances of labels for information box. Displays current update version,
@@ -49,7 +55,7 @@ public class Gui implements ActionListener{
 	protected JLabel multicastAddress = new JLabel("Multicast Address: ");
 	protected JLabel directoryAddress = new JLabel("Directory Address: ");
 	protected JLabel delayTextBox = new JLabel("Delay");
-	
+
 	/**
 	 * Instance variables for menu
 	 */
@@ -61,45 +67,48 @@ public class Gui implements ActionListener{
 	protected JMenuItem menuDirectory = new JMenuItem("Manage Shared Folder");
 	protected JMenuItem menuDelayUpdate = new JMenuItem("Manage Delay Settings");
 	protected JMenuItem quit = new JMenuItem("Exit Program");
-	
+
 	/**
 	 * Instance of other GUI's
 	 */
 	protected FriendGui friend = new FriendGui();
 	protected FolderGui folder = new FolderGui();
 	protected DelayGui delayGui = new DelayGui();
-	
+
 	/**
 	 * Instance variables for minimizing to tray
 	 */
 	protected TrayIcon icon;
-    final PopupMenu popup = new PopupMenu();
-    MenuItem exitItem = new MenuItem("Exit Program");
-	
+	final PopupMenu popup = new PopupMenu();
+	MenuItem exitItem = new MenuItem("Exit Program");
+
 	public Gui(SicComponents comp){
 		components = comp;
+		
+		// receive necessary components from settings
 		folder.addressString = components.settings.getDirectory();
 		friend.addressString = components.settings.get_multicastGroup().toString().substring(1);
 		delayGui.delay = components.settings.getDelay();
-		
+
+		// add listener for each button in the other gui's
 		folder.setFolderAddressButton.addActionListener(this);
 		friend.setMulticastAddressButton.addActionListener(this);
 		delayGui.setDelay.addActionListener(this);
-		
+
 		// create system tray icon
 		ImageIcon image = new ImageIcon();
 		try {
 			image = new ImageIcon(new URL("http://i.imgur.com/D8IxL.gif"));
 		}
 		catch(Exception e) {
-			
+
 		}
 		SystemTray st = SystemTray.getSystemTray();
 		icon = new TrayIcon(image.getImage());
 		icon.setImageAutoSize(true);
 		popup.add(exitItem);
 		exitItem.addActionListener(this);
-		
+
 		// create popup menu so user can quit program
 		icon.addActionListener(this);
 		icon.setPopupMenu(popup);
@@ -109,13 +118,13 @@ public class Gui implements ActionListener{
 		catch(Exception e){
 			System.err.println("System tray unsupported");
 		}
-		
+
 		// set gui frame
 		Dimension frameSize = new Dimension(450, 250);
 		myFrame.setSize(frameSize);
 		myFrame.setTitle("SIC");
 		myFrame.setResizable(false);
-		
+
 		// set listener for home button for each of the other GUIs
 		// the home button brings you back to home page (this gui)
 		friend.homeButton.addActionListener(this);
@@ -127,19 +136,19 @@ public class Gui implements ActionListener{
 		Box row1 = Box.createHorizontalBox();
 
 		/**<---------------------Building Menu------------------------>*/
-		
+
 		menuBar.add(menu);
-		
+
 		// add menu item for setting multicast address
 		menu.add(menuMulticast);
 		menuMulticast.addActionListener(this);
 		menu.addSeparator();
-		
+
 		// add menu item for setting directory address
 		menu.add(menuDirectory);
 		menuDirectory.addActionListener(this);
 		menu.addSeparator();
-		
+
 		// add menu item for updating delay time
 		menu.add(menuDelayUpdate);
 		menuDelayUpdate.addActionListener(this);
@@ -157,12 +166,12 @@ public class Gui implements ActionListener{
 		group.add(disableUpdate);
 		menu.add(autoUpdate);
 		menu.add(disableUpdate);
-		
+
 		menu.add(quit);
 		quit.addActionListener(this);
 
 		myFrame.setJMenuBar(menuBar);	
-		
+
 
 		/**<-----------------------Row 1---------------------------> */
 
@@ -180,7 +189,7 @@ public class Gui implements ActionListener{
 		row1.add(info);
 
 		/**<-------------Add everything to main box-------------------> */
-		
+
 		mainBox.add(Box.createVerticalGlue());
 		mainBox.add(row1);
 		mainBox.add(Box.createVerticalGlue());
@@ -214,7 +223,6 @@ public class Gui implements ActionListener{
 			Box col2 = Box.createVerticalBox();
 			Box col3 = Box.createVerticalBox();
 
-
 			/**<-----------------------Column 2---------------------------> */
 
 			addressTextField.setValue(addressString);
@@ -237,80 +245,6 @@ public class Gui implements ActionListener{
 			addressTextField.setPreferredSize(new Dimension(200, 50));
 			addressTextField.setMinimumSize(new Dimension(200, 50));
 			addressTextField.setMaximumSize(new Dimension(200, 50));
-			
-			col2.add(addressBox);
-			col2.add(buttonBox);
-			col2.add(Box.createVerticalGlue());
-
-			/**<-----------------------Column 3---------------------------> */
-
-			// add home button
-			col3.add(Box.createVerticalGlue());
-			homeButton.setMaximumSize(new Dimension(100, 50));
-			homeButton.setMinimumSize(new Dimension(100, 50));
-			homeButton.setPreferredSize(new Dimension(100, 50));
-			col3.add(homeButton);
-
-			/**<-------------Add everything to main box-------------------> */
-			
-			mainBox.add(col1);
-			mainBox.add(Box.createHorizontalGlue());
-			mainBox.add(col2);
-			mainBox.add(Box.createHorizontalGlue());
-			mainBox.add(col3);
-
-			/**<-------------Add everything to top frame-------------------> */
-			myFrame.add(mainBox);
-			myFrame.setVisible(false);
-		}
-
-	}
-
-	class FolderGui{
-
-		protected JButton setFolderAddressButton = new JButton("Update Folder Directory");
-		protected JFrame myFrame = new JFrame();
-		protected JButton homeButton = new JButton("Home");
-		protected String addressString;// = components.settings.getDirectory();
-		protected JFormattedTextField directoryAddress = new JFormattedTextField();
-
-		public FolderGui(){
-			Dimension frameSize = new Dimension(450, 250);
-			myFrame.setSize(frameSize);
-			myFrame.setTitle("Manage Shared Directory");
-			myFrame.setResizable(false);
-
-
-			Box mainBox = Box.createHorizontalBox();
-			Box col1 = Box.createVerticalBox();
-			Box col2 = Box.createVerticalBox();
-			Box col3 = Box.createVerticalBox();
-
-			/**<-----------------------Column 1---------------------------> */
-
-			/**<-----------------------Column 2---------------------------> */
-
-			directoryAddress.setValue(addressString);
-
-			// add button to set folder address
-			col2.add(Box.createVerticalGlue());
-			Box buttonBox = Box.createHorizontalBox();
-			buttonBox.add(Box.createHorizontalGlue());
-			buttonBox.add(setFolderAddressButton);
-			buttonBox.add(Box.createHorizontalGlue());
-			setFolderAddressButton.setMaximumSize(new Dimension(200, 100));
-			setFolderAddressButton.setMinimumSize(new Dimension(200, 100));
-			setFolderAddressButton.setPreferredSize(new Dimension(200, 100));
-
-			
-			// add directory address text field
-			Box addressBox = Box.createHorizontalBox();
-			addressBox.add(Box.createHorizontalGlue());
-			addressBox.add(directoryAddress);
-			addressBox.add(Box.createHorizontalGlue());
-			directoryAddress.setPreferredSize(new Dimension(200, 50));
-			directoryAddress.setMinimumSize(new Dimension(200, 50));
-			directoryAddress.setMaximumSize(new Dimension(200, 50));
 
 			col2.add(addressBox);
 			col2.add(buttonBox);
@@ -326,7 +260,7 @@ public class Gui implements ActionListener{
 			col3.add(homeButton);
 
 			/**<-------------Add everything to main box-------------------> */
-			
+
 			mainBox.add(col1);
 			mainBox.add(Box.createHorizontalGlue());
 			mainBox.add(col2);
@@ -334,231 +268,237 @@ public class Gui implements ActionListener{
 			mainBox.add(col3);
 
 			/**<-------------Add everything to top frame-------------------> */
-
 			myFrame.add(mainBox);
 			myFrame.setVisible(false);
+
 		}
 	}
 
-	class DelayGui{
-
-		protected JButton setDelay = new JButton("Update Delay");
-		protected JFrame myFrame = new JFrame();
-		protected JButton homeButton = new JButton("Home");
-		protected JFormattedTextField delayTime = new JFormattedTextField();
-		protected int delay;
-
-		public DelayGui(){
-			Dimension frameSize = new Dimension(450, 250);
-			myFrame.setSize(frameSize);
-			myFrame.setTitle("Manage Delay Settings");
-			//myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			myFrame.setResizable(false);
-
-
-			Box mainBox = Box.createHorizontalBox();
-			Box col1 = Box.createVerticalBox();
-			Box col2 = Box.createVerticalBox();
-			Box col3 = Box.createVerticalBox();
-
-			/**<-----------------------Column 1---------------------------> */
-
-			/**<-----------------------Column 2---------------------------> */
-
-			// add update delay button
-			col2.add(Box.createVerticalGlue());
-			Box buttonBox = Box.createHorizontalBox();
-			buttonBox.add(Box.createHorizontalGlue());
-			buttonBox.add(setDelay);
-			buttonBox.add(Box.createHorizontalGlue());
-			setDelay.setMaximumSize(new Dimension(200, 100));
-			setDelay.setMinimumSize(new Dimension(200, 100));
-			setDelay.setPreferredSize(new Dimension(200, 100));
-
-			// add delay text field
-			Box addressBox = Box.createHorizontalBox();
-			addressBox.add(Box.createHorizontalGlue());
-			delayTime.setText(delay + "");
-			addressBox.add(delayTime);
-			addressBox.add(Box.createHorizontalGlue());
-			delayTime.setPreferredSize(new Dimension(200, 50));
-			delayTime.setMinimumSize(new Dimension(200, 50));
-			delayTime.setMaximumSize(new Dimension(200, 50));
-			
-			col2.add(addressBox);
-			col2.add(buttonBox);
-			col2.add(Box.createVerticalGlue());
-
-			/**<-----------------------Column 3---------------------------> */
-
-			// add home button
-			col3.add(Box.createVerticalGlue());
-			homeButton.setMaximumSize(new Dimension(100, 50));
-			homeButton.setMinimumSize(new Dimension(100, 50));
-			homeButton.setPreferredSize(new Dimension(100, 50));
-			col3.add(homeButton);
-
-			/**<-------------Add everything to main box-------------------> */
-			
-			mainBox.add(col1);
-			mainBox.add(Box.createHorizontalGlue());
-			mainBox.add(col2);
-			mainBox.add(Box.createHorizontalGlue());
-			mainBox.add(col3);
-
-			/**<-------------Add everything to top frame-------------------> */
-
-			myFrame.add(mainBox);
-			myFrame.setVisible(false);
-			myFrame.setVisible(false);
-		}
-
-	}
-	
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
-		
+
 		/**<----------------------Multicast Gui--------------------------->*/
 		// if multicast menu item is selected, open up multicast gui
 		if(arg0.getSource() == menuMulticast){
-			friend.addressTextField.setValue(friend.addressString);
-			friend.myFrame.setLocation(myFrame.getLocation());
-			friend.myFrame.setVisible(true);
-			myFrame.setVisible(false);
+			openMulticastGui();
 		}
-		
+
 		// set multicast address only if it is a valid one
 		else if(arg0.getSource() == friend.setMulticastAddressButton){
-			String temp = friend.addressString;// = address.getText();
-			try{
-				friend.addressString = friend.addressTextField.getText();
-				components.settings.set_multicastGroup(
-						InetAddress.getByName(friend.addressString.trim()));
-			}
-			catch (Exception e){
-				JOptionPane.showMessageDialog(myFrame,"Invalid IP address: "
-						+ friend.addressString,"AAAAAAAAAAAAAAAAAAAAAAAH!",0);
-				friend.addressString = temp;
-				friend.addressTextField.setText(friend.addressString);
-			}
+			setMulticastAddress();
 		}	
-		
+
 		// in multicast gui, if pressed home, go back to main menu
 		else if (arg0.getSource() == friend.homeButton){
-			myFrame.setLocation(friend.myFrame.getLocation());
-			myFrame.setVisible(true);
-			friend.myFrame.setVisible(false);
-			multicastAddress.setText("Multicast address: " + friend.addressString);
+			leaveMulticastGui();
 		}
-		
+
 		/**<--------------------Directory Gui---------------------------->*/
 
 		// if clicked on directory management in menu, open new gui
 		else if(arg0.getSource() == menuDirectory){
-			myFrame.setVisible(false);
-			folder.directoryAddress.setText(folder.addressString);
-			folder.myFrame.setLocation(myFrame.getLocation());
-			folder.myFrame.setVisible(true);
+			openDirectoryGui();
 		}
-		
+
 		// set directory address
 		else if(arg0.getSource() == folder.setFolderAddressButton){
-			folder.addressString = folder.directoryAddress.getText();
-			components.settings.updateDirectory(folder.addressString);
-			
-//			if (components == null) {
-//				System.out.println("Error 0");
-				if (components.dirMonitor == null) {
-					System.out.println("Error 1");
-				}
-//			}
-
-			components.dirMonitor.test();
-
-			components.dirMonitor.clearVectors();
-			components.dirMonitor.changeRoot(folder.addressString);
-			
-			
-//			Brute force method... and from sherry, she is tiny!
-//			try {
-//				components.dirMonitor = new DirectoryMonitor(Paths.get(folder.addressString), true);
-//			} catch (IOException e) {
-//				e.printStackTrace();
-//			}
+			setDirectoryAddress();
 		}	
-		
+
 		// if in directory management, and pressed home, go to home gui
 		// also set text in home gui to reflect any changes
 		else if(arg0.getSource() == folder.homeButton){
-			myFrame.setLocation(folder.myFrame.getLocation());
-			myFrame.setVisible(true);
-			folder.myFrame.setVisible(false);
-			directoryAddress.setText("Directory address:" + folder.addressString);
+			leaveDirectoryGui();
 		}
 
 		/**<-------------------------Delay Gui-------------------------->*/
 
 		// open delay gui when delay menu is pressed
 		else if(arg0.getSource() == menuDelayUpdate){
-			myFrame.setVisible(false);
-			delayGui.myFrame.setLocation(myFrame.getLocation());
-			delayGui.delayTime.setText(delayGui.delay + "");
-			delayGui.myFrame.setVisible(true);
-			System.err.println("success! delay updated");
-			components.settings.setDelay(delayGui.delay);
+			openDelayGui();
 		}
 
 		// set delay
 		else if(arg0.getSource() == delayGui.setDelay){
-			delayGui.delay = Integer.parseInt(delayGui.delayTime.getText());
-			components.settings.setDelay(delayGui.delay);
+			setDelay();
 		}	
-		
+
 		// update delay string on home page once delay time has been updated
 		else if(arg0.getSource() == delayGui.homeButton){
-			delayGui.myFrame.setVisible(false);
-			delayTextBox.setText("Delay: " + delayGui.delay + " milliseconds");
-			myFrame.setLocation(delayGui.myFrame.getLocation());
-			myFrame.setVisible(true);
+			leaveDelayGui();
 		}
-		
-		/**<-----------------Auto/manual update------------------------>*/
 
-		// set toggle for auto update and manual update
+		/**<-----------------Auto/manual update------------------------>*/
+		
+		// set toggle for auto update
 		else if (arg0.getSource() == autoUpdate){
-			components.settings.set_auto_updates_enabled(true);
+			enableAutoUpdate();
 		}
+		
+		// set toggle to disable update
 		else if (arg0.getSource() == disableUpdate){
-			components.settings.set_auto_updates_enabled(false);
+			disableAutoUpdate();
 		}
-		
+
 		/**<------------------Exit program----------------------------->*/
-		
+
 		// exits when user chooses to exit program
 		else if (arg0.getSource() == quit){
-			System.exit(0);
+			closeProgram();
 		}
-		
+
 		/**<-----------------TrayIcon Stuff---------------------------->*/
+		
 		// when icon is pressed, brings up the gui again
 		else if (arg0.getSource() == icon){
-			myFrame.setVisible(true);
-			myFrame.setState(Frame.NORMAL);
+			unminimizeGui();
 		}
-		
+
 		// right click on icon, chose to quit
 		else if (arg0.getSource() == exitItem){
-			System.exit(0);
+			closeProgram();
 		}
 	}
-	
-//	/** create and startup a SwingDemo */
-//	public static void main(String[] args)
-//	{
-//		//Gui NiceGui = new Gui("c:/desktop", );
-//	}//main
 
-	/** getter method for SicComponents */
+	/**
+	 * helper method for opening multicast gui
+	 */
+	private void openMulticastGui(){
+		friend.addressTextField.setValue(friend.addressString);
+		friend.myFrame.setLocation(myFrame.getLocation());
+		friend.myFrame.setVisible(true);
+		myFrame.setVisible(false);
+	}
+
+	/**
+	 * helper method for setting multicast address in multicast gui
+	 */
+	private void setMulticastAddress(){
+		String temp = friend.addressString;// = address.getText();
+		try{
+			friend.addressString = friend.addressTextField.getText();
+			components.settings.set_multicastGroup(
+					InetAddress.getByName(friend.addressString.trim()));
+			components.netManager.changeGroup();
+		}
+		catch (Exception e){
+			JOptionPane.showMessageDialog(myFrame,"Invalid IP address: "
+					+ friend.addressString,"AAAAAAAAAAAAAAAAAAAAAAAH!",0);
+			friend.addressString = temp;
+			friend.addressTextField.setText(friend.addressString);
+		}
+	}
+
+	/**
+	 * helper method to close multicast gui and return user to home
+	 */
+	private void leaveMulticastGui(){
+		myFrame.setLocation(friend.myFrame.getLocation());
+		myFrame.setVisible(true);
+		friend.myFrame.setVisible(false);
+		multicastAddress.setText("Multicast address: " + friend.addressString);
+	}
+
+	/**
+	 * helper method for opening directory gui
+	 */
+	private void openDirectoryGui(){
+		myFrame.setVisible(false);
+		folder.directoryAddress.setText(folder.addressString);
+		folder.myFrame.setLocation(myFrame.getLocation());
+		folder.myFrame.setVisible(true);
+	}
+
+	/**
+	 * helper method for setting address of directory 
+	 * 
+	 * save the new directory address and updates directory address in settings
+	 */
+	private void setDirectoryAddress(){
+		folder.addressString = folder.directoryAddress.getText();
+		components.settings.updateDirectory(folder.addressString);
+
+
+		if (components.dirMonitor == null) {
+			System.out.println("Error 1");
+		}
+
+		components.dirMonitor.test();
+		components.dirMonitor.clearVectors();
+		components.dirMonitor.changeRoot(folder.addressString);
+	}
+	
+	/**
+	 * helper method for closing directory gui and returns user to home
+	 */
+	private void leaveDirectoryGui(){
+		myFrame.setLocation(folder.myFrame.getLocation());
+		myFrame.setVisible(true);
+		folder.myFrame.setVisible(false);
+		directoryAddress.setText("Directory address:" + folder.addressString);
+	}
+	
+	/** 
+	 * helper method to open delay gui
+	 */
+	private void openDelayGui(){
+		myFrame.setVisible(false);
+		delayGui.myFrame.setLocation(myFrame.getLocation());
+		delayGui.delayTime.setText(delayGui.delay + "");
+		delayGui.myFrame.setVisible(true);
+		components.settings.setDelay(delayGui.delay);
+	}
+	
+	/**
+	 * helper method that saves the delay and updates the delay in settings
+	 */
+	private void setDelay(){
+		delayGui.delay = Integer.parseInt(delayGui.delayTime.getText());
+		components.settings.setDelay(delayGui.delay);
+	}
+	
+	/**
+	 * helper method that leaves the delay gui and returns user to home
+	 */
+	private void leaveDelayGui(){
+		delayGui.myFrame.setVisible(false);
+		delayTextBox.setText("Delay: " + delayGui.delay + " milliseconds");
+		myFrame.setLocation(delayGui.myFrame.getLocation());
+		myFrame.setVisible(true);
+	}
+	
+	/**
+	 * helper method that enables auto updates in settings
+	 */
+	private void enableAutoUpdate(){
+		components.settings.set_auto_updates_enabled(true);
+	}
+	
+	/**
+	 * helper method that disables auto updates in settings
+	 */
+	private void disableAutoUpdate(){
+		components.settings.set_auto_updates_enabled(false);
+	}
+	
+	/**
+	 * helper method that brings back the gui once the tray icon is pressed
+	 */
+	private void unminimizeGui(){
+		myFrame.setVisible(true);
+		myFrame.setState(Frame.NORMAL);
+	}
+	
+	/**
+	 * helper method for closing program
+	 */
+	private void closeProgram(){
+		System.exit(0);
+	}
+	
+	/** 
+	 * getter method for SicComponents 
+	 */
 	public void setComponents(SicComponents comp){
 		components = comp;
 	}
